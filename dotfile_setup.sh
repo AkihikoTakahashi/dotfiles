9bb40dd6a8be52530174db64addc33bd7f9b1280
AkihikoTakahashi <fermatslasttheoremisdifficult@gmail.com>
2022-05-24 10:41:25 +0900

    remove ~/.emacs.d if exist

--- dotfile_setup.sh
+++ dotfile_setup.sh
@@ -12,5 +12,8 @@ echo setup .yatex-template
 ln -sf ${cur_dir}/.yatex-template ~/
 
 echo setup .emacs.d
+if [ ! -d ~/.emacs.d ]; then
+  rm -rf ~/.emacs.d
+fi
 ln -sf ${cur_dir}/.emacs.d ~/
 emacs --batch -f batch-byte-compile ~/.emacs.d/init.el
